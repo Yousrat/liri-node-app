29f911076046d35681bb4e2db93d1ce4b579d396
Yousrat <ybanna@gmail.com>
2017-03-26 19:51:53 -0700

    Adding .gitignore file

---
+++ liri.js
@@ -0,0 +1,7 @@
+var request = require("request");
+var spotify = require("spotify");
+var twitter = require("twitter");
+
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
+});
